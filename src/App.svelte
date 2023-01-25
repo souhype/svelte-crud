a6a997972db142f020e7035f677843079766524e
souype <souhype@protonmail.com>
2023-01-25 13:40:26 +0100

    .

--- src/App.svelte
+++ src/App.svelte
@@ -69,57 +69,92 @@
 </script>
 
 <main>
-  <section>
-    <form on:submit|preventDefault={addItem}>
-      <input
-        pattern="[a-zA-Z ]+"
-        minlength="5"
-        maxlength="15"
-        placeholder="Your Task"
-        required
-        bind:value
-        bind:this={input}
-      />
-    </form>
-  </section>
+  <h1>To Do App</h1>
+  <form on:submit|preventDefault={addItem}>
+    <input
+      pattern="[a-zA-Z ]+"
+      minlength="5"
+      maxlength="15"
+      placeholder="Your Task"
+      required
+      bind:value
+      bind:this={input}
+    />
+  </form>
   {#if $list.length}
     <section>
       <input type="button" on:click={showAll} value={"all"} />
       <input type="button" on:click={showActive} value={"active"} />
       <input type="button" on:click={showChecked} value={"checked"} />
     </section>
-    <section>
-      <ul>
-        {#each filteredList as { id, name, checked, readonly } (id)}
-          <li>
-            <section>
-              <input bind:checked type="checkbox" />
-              <input id={id.toString()} bind:value={name} {readonly} />
-              <input
-                class="edit"
-                on:click={() => editItem(id, readonly)}
-                type="button"
-                value={readonly ? "edit" : "confirm"}
-              />
-              <input
-                class="remove"
-                on:click={() => removeItem(id)}
-                type="button"
-                value="remove"
-              />
-            </section>
-          </li>
-        {/each}
-      </ul>
-    </section>
+    <ul>
+      {#each filteredList as { id, name, checked, readonly } (id)}
+        <li>
+          <input bind:checked type="checkbox" />
+          <input id={id.toString()} bind:value={name} {readonly} />
+          <input
+            class="edit"
+            on:click={() => editItem(id, readonly)}
+            type="button"
+            value={readonly ? "edit" : "confirm"}
+          />
+          <input
+            class="remove"
+            on:click={() => removeItem(id)}
+            type="button"
+            value="remove"
+          />
+        </li>
+      {/each}
+    </ul>
     <section>
       <input type="button" on:click={checkAll} value={"check all"} />
       <input type="button" on:click={uncheckAll} value={"uncheck all"} />
       <input type="button" on:click={removeChecked} value={"remove checked"} />
     </section>
   {:else}
-    <section>
-      <h1>Nothing to see here</h1>
-    </section>
+    <h1>Nothing to see here</h1>
   {/if}
 </main>
+
+<style>
+  main {
+    display: grid;
+    place-items: center;
+    gap: 1rem;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+    list-style-type: none;
+    display: grid;
+    gap: 1rem;
+  }
+
+  li {
+    display: grid;
+    grid-template-columns: 0.25fr 1fr auto auto;
+  }
+
+  input {
+    margin: 0.25rem;
+    padding: 0.5rem 1rem;
+    border-radius: 8px;
+    border: none;
+    outline: 0;
+    cursor: pointer;
+    font: inherit;
+    text-transform: capitalize;
+    color: inherit;
+    background: hsl(0 0% 20%);
+  }
+
+  .edit {
+    background: hsl(250 50% 50%);
+  }
+
+  .remove {
+    background: hsl(0 50% 50%);
+  }
+</style>
